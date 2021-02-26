a559c5bd44ebbbdceb5a78c5edcd4b9685ec0b4a
lcicmp <lcicmp@163.com>
2021-02-26 16:16:31 +0800

    master

---
+++ ewshop/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="~assets/logo.png">
+    <img alt="Vue logo" :src="imgsrc">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '~components/HelloWorld.vue'//@代表src
+
+export default {
+  name: 'Home',
+  data(){
+    return {
+      imgsrc:require('assets/images/2.png')
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
